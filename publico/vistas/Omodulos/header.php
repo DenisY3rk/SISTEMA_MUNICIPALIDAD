86d6a4071fad9aaa563c9033db7f840f26062e09
DenisY3rk2 <39609171+DenisY3rk@users.noreply.github.com>
2021-07-28 08:54:21 -0500

    Update header.php

--- publico/vistas/Omodulos/header.php
+++ publico/vistas/Omodulos/header.php
@@ -28,28 +28,6 @@
               <li><a href="directoriofuncionarios">Directorio de Funcionarios</a></li>
             </ul>
           </li>
-          <li class="dropdown"><a href="<?php echo $url; ?>"><span><i class="bi bi-file-text"></i>&nbsp Gestión Municipal</span> <i class="bi bi-chevron-down"></i></a>
-            <ul>
-              <li class="dropdown"><a href="#"><span>Normas Emitidas</span> <i class="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Acuerdos de Consejo</a></li>
-                  <li><a href="#">Decretos de Alcaldia</a></li>
-                  <li><a href="#">Ordenanzas Municipales</a></li>
-                  <li><a href="#">Resoluciones de Alcaldia</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Reporte de Procesos Judiciales</a></li>
-              <li><a href="#">Saneamiento de Inmuebles</a></li>
-              <li><a href="informacion-legal">Información Legal</a></li>
-              <li><a href="#">Resoluciones de Gerencia Municipal</a></li>
-              <li><a href="#">Resoluciones de Gerencia de Administracion</a></li>
-              <li><a href="#">Resoluciones de la Sub Gerencia de Personal</a></li>
-              <li><a href="#">Sistema de control Interno</a></li>
-              <li><a href="#">Avisos Oficiales</a></li>
-              <li><a href="#">Canasta Basica Familiar</a></li>
-              <li><a href="#">Resoluciones de Demuna</a></li>
-            </ul>
-          </li>
           <li><a class="admin" href="<?php echo $servidor; ?>"><i class="bi bi-person-circle"></i>&nbsp Iniciar Sesión</a></li>
           </ul>
         <i class="bi bi-list mobile-nav-toggle"></i>
