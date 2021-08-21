728ca44a031bc0ca61a72e9c33887f8b6cefa3d8
DenisY3rk <39609171+DenisY3rk@users.noreply.github.com>
2021-08-20 21:06:46 -0500

    Update topBar.php

--- publico/vistas/Omodulos/topBar.php
+++ publico/vistas/Omodulos/topBar.php
@@ -2,8 +2,9 @@
   <section id="topbar" class="d-flex align-items-center">
     <div class="container d-flex justify-content-center justify-content-md-between">
       <div class="contact-info d-flex align-items-center">
-        <i class="bi bi-telephone d-flex align-items-center ms-4"><span>Contacto 1: (051) 000000</span></i>
-        <i class="bi bi-telephone d-flex align-items-center ms-4"><span>Contacto 2: (051) 000000</span></i>
+        <i class="bi bi-telephone d-flex align-items-center ms-4"><span>Contacto 1: (+51) 976338770</span></i>
+        <i class="bi bi-telephone d-flex align-items-center ms-4"><span>Contacto 2: (+51) 962316629</span></i>
+        <i class="bi bi-mailbox d-flex align-items-center ms-4"><span>E-mail: munisjo2019.2022@gmail.com</span></i>
       </div>
       <div class="social-links d-none d-md-flex align-items-center">
         <a href="https://www.facebook.com/Municipalidad-Distrital-de-San-Juan-del-Oro-302831833781863/" target="_blank" class="facebook"><i class="bi bi-facebook"></i></a>
